7af2488105ff2fc05d03ba4eff68ff5143b12c47
Hugh Hoang <hugh.hoang@gmail.com>
2020-11-07 23:20:05 +1300

    feat: web speech API and Google Translate

--- src/components/SpeechToText.vue
+++ src/components/SpeechToText.vue
@@ -5,7 +5,10 @@
       <i v-else class="fas fa-microphone"></i>
     </span>
     <ul class="chat">
-      <li class="text-other">
+      <li v-for="(text, idx) in results" :key="idx" class="text-me">
+        {{ text }}
+      </li>
+      <!-- <li class="text-other">
         Chịu sao thấu híc híc
       </li>
       <li class="text-me">
@@ -17,25 +20,63 @@
       </li>
       <li class="text-me">
         I had the same issue and the solution was to follow the official docs.
-      </li>
+      </li> -->
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import { v2 as GoogleTranslate } from '@google-cloud/translate'
+
+const SpeechRecognition =
+  window.SpeechRecognition || (window as any).webkitSpeechRecognition
+const SpeechGrammarList =
+  window.SpeechGrammarList || (window as any).webkitSpeechGrammarList
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || (window as any).webkitSpeechRecognitionEvent
+
 export default defineComponent({
   name: 'SpeechToText',
   setup() {
     const open = ref(false)
+    const results = ref<string[]>([])
+
+    const recognition = new SpeechRecognition()
+    recognition.continuous = true
+    recognition.lang = 'en-NZ'
+    recognition.interimResults = false
+    recognition.maxAlternatives = 1
 
     const handleClick = () => {
       open.value = !open.value
+      if (open.value) {
+        recognition.start()
+      } else {
+        recognition.stop()
+      }
     }
+
+    onMounted(() => {
+      const translate = new GoogleTranslate.Translate({
+        key: process.env.VUE_APP_GOOGLE_API_KEY,
+      })
+      recognition.onresult = event => {
+        const latest = event.results[results.value.length]
+        const transcript = latest[0].transcript
+        translate.translate(transcript, 'vi').then(translations => {
+          const trans = Array.isArray(translations)
+            ? translations
+            : [translations]
+          results.value.push(trans[0])
+        })
+      }
+    })
 
     return {
       open,
       handleClick,
+      results,
     }
   },
 })
